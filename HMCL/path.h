8cc5d261609c2f7a343d3c4962687f737460a56f
Glavo <zjx001202@gmail.com>
2025-05-29 02:44:26 +0800

    update

--- HMCL/path.h
+++ HMCL/path.h
@@ -11,11 +11,11 @@ struct HLPath {
   explicit(false) HLPath(std::wstring path) : path(std::move(path)) {}
   explicit(false) HLPath(const wchar_t* path) : path(path) {}
 
-  void AddBackslash();
-
-  HLPath operator/(const std::wstring& append) const;
-
-  void operator/=(const std::wstring& append);
+  HLPath &operator=(const HLPath& other) = default;
 
   bool IsRegularFile() const;
+
+  void AddBackslash();
+  HLPath operator/(const std::wstring& append) const;
+  void operator/=(const std::wstring& append);
 };
