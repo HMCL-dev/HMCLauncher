83d8b0b13301bc93521356870beb1306c7bc7b68
Glavo <zjx001202@gmail.com>
2025-07-12 01:52:34 +0800

    将最低 Java 版本提升至 Java 11

--- HMCL/i18n.cpp
+++ HMCL/i18n.cpp
@@ -8,7 +8,7 @@ HLI18N HLI18N::Instance() {
                      L"The Java path specified by HMCL_JAVA_HOME is invalid. Please update it to a valid Java "
                      "installation path or remove this environment variable.",
                  .errorJavaNotFound =
-                     L"The Java runtime environment is required to run HMCL and Minecraft,\n"
+                     L"HMCL requires Java 11 or higher to run,\n"
                      L"Click 'OK' to start downloading java.\n"
                      L"Please restart HMCL after installing Java."};
 
@@ -17,7 +17,7 @@ HLI18N HLI18N::Instance() {
     i18n.errorSelfPath = L"获取程序路径失败。";
     i18n.errorInvalidHMCLJavaHome = L"HMCL_JAVA_HOME 所指向的 Java 路径无效，请更新或删除该变量。\n";
     i18n.errorJavaNotFound =
-        L"运行 HMCL 以及 Minecraft 需要 Java 运行时环境，点击“确定”开始下载。\n"
+        L"HMCL 需要 Java 11 或更高版本才能运行，点击“确定”开始下载 Java。\n"
         L"请在安装 Java 完成后重新启动 HMCL。";
   }
   return i18n;
